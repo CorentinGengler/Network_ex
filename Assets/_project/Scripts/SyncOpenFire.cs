a8c83d49d8187f2e151c5512506a180d61b100e5
CorentinGengler <gengcor@gmail.com>
2018-01-25 13:34:27 +0100

    Commit1

---
+++ Assets/_project/Scripts/SyncOpenFire.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+
+public class SyncOpenFire  : NetworkBehaviour
+{
+
+    #region Public Members
+    public GameObject m_bulletPrefab;
+    public Transform m_spawnTransform;
+
+    public float m_minForce = 2f;
+    public float m_maxForce = 20f;
+    public float m_timeBeforeAutoDestruct = 5f;
+    [Range(0, 1)]
+    public float m_forceApplied = 0.5f;
+
+    public GameObject m_instanciatedBullet;
+    
+    #endregion
+
+
+    #region Public Void
+    public void SetForceApplied(float force)
+    {
+        m_forceApplied = force;
+    }
+
+    public void Fire()
+    {
+        CmdFire(m_forceApplied);
+    }
+    [Command]
+    void CmdFire(float forceApplied)
+    {
+        if (m_instanciatedBullet == null)
+        {
+            GameObject bullet = Instantiate(m_bulletPrefab, m_spawnTransform.position, m_spawnTransform.rotation);
+            m_instanciatedBullet = bullet;
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * Mathf.Lerp(m_minForce, m_maxForce, forceApplied);
+            NetworkServer.Spawn(bullet);
+            Destroy(bullet, m_timeBeforeAutoDestruct);
+        }
+    }
+    #endregion
+
+
+    #region System
+
+
+    void Update () 
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("Fire!");
+            Fire();
+        }
+    }
+    
+    #endregion
+
+    #region Private Void
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+
+    #region Private And Protected Members
+
+    #endregion
+
+}
---
+++ Assets/_project/Scripts/SyncPlayerInformation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SyncPlayerInformation  : NetworkBehaviour
+{
+
+    #region Public Members
+    [SyncVar]
+    public string m_name;
+    
+    public TextMesh m_displayPlayerName;
+
+    public InputField m_fieldToHide;
+
+    public void SetNameTo(string name)
+    {
+        CmdChangeName(name);
+        m_fieldToHide.gameObject.SetActive(false);
+        m_displayPlayerName.text = name;
+    }
+
+    [Command]
+    public void CmdChangeName(string name)
+    {
+        Debug.Log("SERVER CHANGE NAME : " + name);
+        m_name = name;
+        m_displayPlayerName.text = name;
+    }
+    #endregion
+
+
+    #region Public Void
+
+    #endregion
+
+
+    #region System
+
+
+    #endregion
+
+    #region Private Void
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+
+    #region Private And Protected Members
+
+    #endregion
+
+}
